d3b47f3696ed8d2a6ee0a8bcbe35bb043c7f0be4
rocket <gamingdas@gmail.com>
2021-11-08 05:37:29 +0300

    (#113) Some refactoring attempt

---
+++ src/main/java/org/fusionsoft/database/snapshot/objects/resultset/ObjectOfEntries.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018-2021 FusionSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.fusionsoft.database.snapshot.objects.resultset;
+
+import com.amihaiemil.eoyaml.YamlMapping;
+import com.amihaiemil.eoyaml.YamlNode;
+import java.util.Map;
+import org.cactoos.Func;
+import org.cactoos.Text;
+import org.cactoos.func.UncheckedFunc;
+import org.fusionsoft.database.snapshot.objects.ObjectType;
+import org.fusionsoft.database.snapshot.objects.SimpleDbObject;
+import org.fusionsoft.database.snapshot.objectsignature.FullObjectName;
+import org.fusionsoft.database.snapshot.objectsignature.SimpleObjectSignature;
+import org.fusionsoft.lib.yaml.MappingWithoutNullScalars;
+import org.fusionsoft.lib.yaml.YamlMappingOfEntries;
+
+public class ObjectOfEntries<T extends YamlMapping> extends SimpleDbObject<T> {
+
+    /**
+     * Instantiates a new simple db object.
+     */
+    public ObjectOfEntries(
+        final ObjectType type,
+        final Func<YamlMapping, T> ctor,
+        final Iterable<Text> signature,
+        final Iterable<? extends Map.Entry<? extends Text, ? extends YamlNode>> entries
+    ) {
+        super(
+            new UncheckedFunc<>(ctor).apply(
+                new MappingWithoutNullScalars(
+                    new YamlMappingOfEntries(
+                        entries
+                    )
+                )
+            ),
+            new SimpleObjectSignature(new FullObjectName(signature), type)
+        );
+    }
+
+}
