9d9393a0cc8e7ff9b788ac3e4a15316378add7ff
rocket <gamingdas@gmail.com>
2021-12-03 19:40:05 +0300

    (#106) Implemented classes for fetching Objects<DbdDataMapping> with links to separate data file

---
+++ src/main/java/org/fusionsoft/database/snapshot/data/InlineLinkDataMappingOfTable.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2018-2021 FusionSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.
+ *
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.fusionsoft.database.snapshot.data;
+
+import org.fusionsoft.database.mapping.dbd.DbdTableMapping;
+import org.fusionsoft.database.mapping.dbd.built.DbdDataMappingOfValue;
+import org.fusionsoft.database.snapshot.DbObject;
+
+public class InlineLinkDataMappingOfTable extends DbdDataMappingOfValue {
+
+    public InlineLinkDataMappingOfTable(
+        final DbObject<DbdTableMapping> table
+    ) {
+        super(
+            new SeparateDataFilePathRelativeToDbd(
+                new SeparateDataFileName(table)
+            )
+        );
+    }
+
+}
